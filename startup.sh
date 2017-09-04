edc4b33e0e186df99174b560dfb8099fc78be171
Vladimir Baranov <330034@gmail.com>
2017-09-04 11:35:27 +0600

    addscripts2

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git clone https://github.com/vsb2007/infra.git
+git clone https://github.com/vsb2007/infra2.git
 
-cd infra
+cd infra2
 bash install_ruby.sh
 bash install_mongodb.sh
 bash deploy.sh
